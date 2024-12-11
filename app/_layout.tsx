382fc88ac520d2cf242cef17f4fac628340dcc8f
MennaEbied <mennahesham253@gmail.com>
2024-12-11 22:29:16 +0200

    Update layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,27 @@ import { Stack } from "expo-router";
 export default function Layout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown:false }} />
+      <Stack.Screen
+        name="index"
+        options={{ headerShown: false, animation: "fade" }}
+      />
+      <Stack.Screen
+        name="onboarding/FirstOnboarding"
+        options={{ headerShown: false, animation: "fade" }}
+      />
+      <Stack.Screen
+        name="onboarding/SecondOnboarding"
+        options={{ headerShown: false, animation: "fade" }}
+      />
+       <Stack.Screen
+        name="authentication/SignIn"
+        options={{ headerShown: false, animation: "fade" }}
+      />
+      <Stack.Screen
+        name="authentication/SignUp"
+        options={{ headerShown: false, animation: "fade" }}
+      />
+     
     </Stack>
   );
 }
