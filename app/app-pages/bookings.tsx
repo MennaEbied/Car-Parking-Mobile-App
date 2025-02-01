4000ae4321e12944496ee654340d934cef56ca33
Maha <amaha5025@gmail.com>
2025-02-02 00:57:28 +0200

    edit booking page

--- app/app-pages/bookings.tsx
+++ app/app-pages/bookings.tsx
@@ -4,33 +4,27 @@ import { router } from "expo-router";
 
 const ParkingSlots = () => {
   const slots = [
-    { id: 'A1', image: { uri: 'https://i.pinimg.com/474x/09/93/ef/0993ef778f6b7bb80621fbd96d2c837a.jpg' } },
-    { id: 'A6', image: { uri: 'https://i.pinimg.com/474x/64/ba/04/64ba04d7c3b531ee14a4ffb35a4a619e.jpg' }},
-    { id: 'A2',  },
-    { id: 'A7', },
+    { id: 'A1' ,image: { uri: 'https://i.pinimg.com/474x/09/93/ef/0993ef778f6b7bb80621fbd96d2c837a.jpg' } },
+    { id: 'A6' ,image: { uri: 'https://i.pinimg.com/474x/64/ba/04/64ba04d7c3b531ee14a4ffb35a4a619e.jpg' }},
+    { id: 'A2' },
+    { id: 'A7'},
     { id: 'A3',image:{uri:'https://i.pinimg.com/474x/d4/93/e4/d493e4198352e55cc829ab1460191df9.jpg'} },
-    { id: 'A8', },
-    { id: 'A4',  },
+    { id: 'A8' },
+    { id: 'A4' },
     { id: 'A9',image:{uri:'https://i.pinimg.com/474x/d6/ac/d1/d6acd1ad0e61c8e6f51ca455b84c48a8.jpg'}  },
     { id: 'A5',image:{uri:'https://i.pinimg.com/474x/28/f3/10/28f310fec909f709058408b2edc2543e.jpg'}  },
-    { id: 'A10', },
+    { id: 'A10'},
   ];
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Parking Slots!</Text>
       <View style={styles.slotsContainer}>
         {slots.map((slot,index) => (
-          <View key={slot.id} style={styles.slotsContainer}>
-          <View
-            style={[
-              styles.slot,
-            ]}
-          >
-            <Image source={slot.image} style={styles.image} ></Image>
-            <Text style={styles.slotLabel}>{slot.id}</Text>
-          </View>
+          <View key={slot.id} style={styles.slot}>
+          <Image source={slot.image} style={styles.image} />
+          <Text style={styles.slotLabel}>{slot.id}</Text>
           {index < slots.length  && <View style={styles.line} />}
-          </View>
+        </View>
         ))}
         <Pressable
             style={styles.button}
@@ -48,7 +42,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
     alignItems: 'center',
-    justifyContent: "center",
+    justifyContent: "space-between",
     padding:15,
   },
   title: {
