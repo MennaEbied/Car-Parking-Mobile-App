89005f95e0ceff9d7d1e405f24313b3dc22b8a4c
Maha <amaha5025@gmail.com>
2025-02-07 21:49:20 +0200

    edit home page

--- app/app-pages/home.tsx
+++ app/app-pages/home.tsx
@@ -1,118 +1,62 @@
 import React from 'react';
-import {View,Text,StyleSheet,ScrollView} from 'react-native';
+import { View, Text, TextInput, Pressable, StyleSheet, ImageBackground} from 'react-native';
+import { router } from "expo-router";
 
-const slots = [
-  { id: 1, status: 'available', plate: 'ABC-123' },
-  { id: 2, status: 'available', plate: 'ABC-124' },
-  { id: 3, status: 'occupied', plate: 'ABC-125' },
-  { id: 4, status: 'available', plate: 'ABC-126' },
-  { id: 5, status: 'reserved', plate: 'ABC-127' },
-  { id: 6, status: 'occupied', plate: 'ABC-128' },
-  { id: 7, status: 'available', plate: 'ABC-129' },
-  { id: 8, status: 'available', plate: 'ABC-130' },
-  { id: 9, status: 'occupied', plate: 'ABC-131' },
-  { id: 10, status: 'reserved', plate: 'ABC-132' },
-];
-
-const ParkingSlots: React.FC = () => {
+const HomePage: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title1}> Welcome! </Text>
-      <Text style={styles.title2}> Find best parking slot </Text>
-      <View style={styles.legendContainer}>
-      <View style={[styles.legendItem, styles.available]} />
-      <Text style={styles.legendText}>Available</Text>
-      <View style={[styles.legendItem, styles.occupied]} />
-      <Text style={styles.legendText}>Occupied</Text>
-      <View style={[styles.legendItem, styles.reserved]} />
-      <Text style={styles.legendText}>Reserved</Text>
-    </View>
-      <ScrollView contentContainerStyle={styles.slotContainer}>
-        {slots.map((slot) => (
-          <View
-          key={slot.id}
-          style={[
-            styles.slot,
-            slot.status === "available" && styles.available,
-            slot.status === "occupied" && styles.occupied,
-            slot.status === "reserved" && styles.reserved,
-          ]}
-        >
-          <Text style={styles.slotText}>SLOT-{slot.id}</Text>
-          <Text style={styles.plateText}>{slot.plate}</Text>
+    <ImageBackground
+    source={require("../../assets/home-img.jpg")}
+      style={styles.background}
+      resizeMode="cover"
+    >
+      <View style={styles.container}>
+        <View>
+          <TextInput
+            style={styles.searchInput}
+            placeholder="Search"
+            placeholderTextColor="#888"
+          />
         </View>
-        ))}
-      </ScrollView>
-    </View>
+        <Text style={styles.text}>Hello, ....</Text>
+         <Pressable onPress={()=> router.push("pages/slots")} >
+                    <Text style={styles.buttonText}>Plan parking now</Text>
+            </Pressable>
+            <View/>
+      </View>
+    </ImageBackground>
   );
 };
-
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: '#ffffff',
-  },
-  title1: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    marginBottom: 10,
-    marginTop:50,
-  },
-  title2: {
-    fontSize: 12,
-    marginBottom: 40,
-  },
-  legendContainer: {
-    flexDirection: 'row',
-    alignItems: "center",
-    justifyContent: "center",
-    marginBottom: 30,
-  },
-  legendItem: {
-    width: 15,
-    height: 15,
-    borderRadius: 10,
-    marginRight: 2, 
-  },
-  available: {
-    backgroundColor: "#449d48",
-  },
-  occupied: {
-    backgroundColor: "#f32c1e",
-  },
-  reserved: {
-    backgroundColor: "#ffe608",
-  },
-  legendText: {
-    fontSize: 15,
-    marginRight: 15,
-  },
-  slotContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    alignItems: 'center',
-    justifyContent: "space-between"
-  },
-  slot: {
-    width: '45%',
-    height: 90,
-    borderRadius: 5,
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 20,
-    marginHorizontal:7
   },
-  slotText: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#fff',
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: 'center',
   },
-  plateText: {
-    fontSize: 12,
+  searchInput: {
+    height: 40,
+    width:300,
+    marginTop:50,
+    borderRadius: 20,
+    borderColor: "black",
+    borderWidth: 1,
+    paddingLeft:15,
+    backgroundColor:"white",
+  },
+  text: {
+    fontSize: 25,
+    marginBottom:400,
+    marginRight:120
+  },
+  buttonText: {
     color: '#fff',
+    fontSize: 16,
+    marginLeft:120,
+    marginTop:60
   },
 });
-
-export default ParkingSlots;
+export default HomePage;
